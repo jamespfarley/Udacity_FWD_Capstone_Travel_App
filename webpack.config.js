a80f215899fe5d5e8bf29240eabd11a206d9e35c
lonestar <jimfarley003@gmail.com>
2020-06-22 12:48:25 -0400

    Chore:  installed 'dotenv-webpack' to enable the use of env variables in javascript

--- webpack.config.js
+++ webpack.config.js
@@ -4,6 +4,7 @@ const webpack = require('webpack');
 const htmlWebpackPlugin = require("html-webpack-plugin");
 const { CleanWebpackPlugin } = require('clean-webpack-plugin');
 const ServiceWorkerWebpackPlugin = require('serviceworker-webpack-plugin');
+const Dotenv = require('dotenv-webpack');
 
 module.exports = {
     entry: './src/client/index.js', 
@@ -42,6 +43,7 @@ module.exports = {
         }),
         new ServiceWorkerWebpackPlugin({
             entry: path.join(__dirname, 'src/sw.js'),
-          })
+          }),
+        new Dotenv()
     ]
-}
+};
