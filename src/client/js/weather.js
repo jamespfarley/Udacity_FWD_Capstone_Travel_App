6c0dcddc3f75c4ed886119c09592edd628fa13f6
lonestar <jimfarley003@gmail.com>
2020-06-26 15:13:00 -0400

    refactor:  Declare weatherbitBaseURL locally

--- src/client/js/weather.js
+++ src/client/js/weather.js
@@ -1,8 +1,9 @@
-const weatherbitBaseURL = `https://api.weatherbit.io/v2.0/forecast/daily?key=${process.env.WB_API_KEY}&lang=en&units=I&days=1`;
 
 // Get weather data
 export const getWeatherData = async (data) => {
-    
+
+            const weatherbitBaseURL = `https://api.weatherbit.io/v2.0/forecast/daily?key=${process.env.WB_API_KEY}&lang=en&units=I&days=1`;
+
             let URL = `${weatherbitBaseURL}&city=${data.geonames[0].name}&country=${data.geonames[0].countryCode}`;
 
             const response = await fetch(URL).catch( error => { console.log(`getWeatherData fetch() error: ${error}`)});
