6c63c7ce1c08bb50a16d57c507cb2d2bca4ef4dc
lonestar <jimfarley003@gmail.com>
2020-06-26 09:51:05 -0400

    feat:  Pixabay API
    Create API to request image data from Pixabay

--- src/server/index.js
+++ src/server/index.js
@@ -27,7 +27,10 @@ app.use(express.static('dist'));
 const path = require('path');
 
 // Setup empty JS object to act as endpoint for all routes
-const projectData = {};
+const destinationData = {};
+
+// To make HTTP calls
+const request = require('request');
 
 // Callback function to serve homepage
 app.get('/destination', (req, res) => {
@@ -36,24 +39,59 @@ app.get('/destination', (req, res) => {
     res.sendFile(path.join(__dirname + "../../../dist/index.html"))
 });
 
+
 // Callback function to serve returned data
 app.get('/all', (req, res) =>{
-    res.send(projectData);
+    res.send(destinationData);
 })
 
 // Post Route
 app.post('/destination', addInfo);
-
 function addInfo(req, res){
     // !!!
     console.log('... app.post() :: addInfo : req.body = ' + JSON.stringify(req.body));
 
-    Object.assign(projectData, req.body);
+    Object.assign(destinationData, req.body);
 
     // !!!
-    console.log('... app.post() :: addInfo : projectData = ' + JSON.stringify(projectData));
+    console.log('... app.post() :: addInfo : destinationData = ' + JSON.stringify(destinationData));
 
-    res.send(projectData);
+    res.send(destinationData);
 }
+
+/*** Create proxy-server for pixabay ******************************************************/
+
+const imgParam = {};
+
+app.use((req, res, next) => {
+    res.header('Access-Control-Allow-Origin', '*');
+    next();
+});
+
+app.get('/pixabay', (req, res) => {
+    let pixabayURL = "https://pixabay.com/api/?key="
+                     + process.env.PIXBY_API_KEY
+                     + "&lang=en&category=travel&image_type=photo&orientation=vertical&q="
+                     + imgParam.city;
+
+    console.log(`... index.js : /pixabay :: url = ${pixabayURL}`);
+
+    request({url: pixabayURL}, (error, response, body) =>{
+        if (error || response.statusCode !== 200) {
+            return res.status(500).json({type: 'error', message: error.message});
+        }
+
+        res.json(JSON.parse(body));
+    })
+})
+
+app.post('/pixabay', (req, res) => {
+    
+    Object.assign(imgParam, req.body);
+    res.send(imgParam);
+})
+
+/******************************************************************************************/
+
 
 module.exports = app;
